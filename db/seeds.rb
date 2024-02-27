3f6081fa17363067129cfc859b418b86acc4caef
stefanseeholzer <stefanseeholzer99@gmail.com>
2024-02-27 14:48:39 -0600

    finished seed file getting data from csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,47 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+require "csv"
+
 # product = Product.create
 # puts product.errors.full_messages
 
-676.times do
+# 676.times do
+#     product = Product.create(
+#         title: Faker::Commerce.product_name,
+#         price: Faker::Commerce.price(range: 0..10.0, as_string: true),
+#         stock_quantity: Faker::Number.between(from: 0, to: 100)
+#     )
+# end
+
+# puts "Created #{Product.count} products"
+
+Product.destroy_all
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+    title = product['name']
+    description = product['description']
+    price = product['price']
+    stock_quantity = product['stock quantity']
+    category_name = product['category']
+
+    category = Category.find_or_create_by(name: category_name)
+
     product = Product.create(
-        title: Faker::Commerce.product_name,
-        price: Faker::Commerce.price(range: 0..10.0, as_string: true),
-        stock_quantity: Faker::Number.between(from: 0, to: 100)
+        title: title,
+        description: description,
+        price: price,
+        stock_quantity: stock_quantity,
+        category: category,
     )
+    puts product.errors.full_messages
 end
 
 puts "Created #{Product.count} products"
+puts "Created #{Category.count} Categories"
